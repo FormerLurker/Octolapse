95497d0f2271b3db364d8b1d88776393c630c204
ninjamojo <brad@ninjamojo.com>
2017-11-04 20:40:43 -0500

    Fix issues with settings structure

--- octoprint_octolapse/__init__.py
+++ octoprint_octolapse/__init__.py
@@ -17,79 +17,91 @@ class OctolapsePlugin(octoprint.plugin.SettingsPlugin,
 	##~~ SettingsPlugin mixin
 
 	def get_settings_defaults(self):
-		return dict(enabled = True,
-				    selected_profile_index = 1,
-					profiles= {
-						dict(name= "default",
-						description= "Fixed XY at back left - relative stabilization (0,100)",
-						printer = dict(bed_x_max= -1, 
-							bed_y_max= -1, 
-							bed_z_max= -1,),
-						stabilization = dict(x_movement_speed= 0,
-							x_type = 'fixed',
-							x_fixed_coordinate = 0,
-							x_fixed_path= {},
-							x_fixed_path_loop= True,
-							x_relative= 100,
-							x_relative_print= 100,
-							x_relative_path= {},
-							x_relative_path_loop= True,
-							y_movement_speed_mms= 0, 
-							y_type = 'fixed',
-							y_fixed_coordinate = 0,
-							y_fixed_path= {},
-							y_fixed_path_loop= True,
-							y_relative= 100,
-							y_relative_print= 100,
-							y_relative_path= {},
-							y_relative_path_loop= True,
-							z_movement_speed_mms= 0, # auto
-						),
-						snapshot = dict(trigger_type = 'layer_change',
-							length = 0.2,
-							seconds = 30,
-							archive = True,
-							delay = 1000,
-							retract_before_move= False),
-						rendering = dict(enabled = True,
-							fps_calculation_type = 'duration',
-							run_length_seconds = 10,
-							fps = 30,
-							max_fps = 120.0,
-							min_fps = 1.0,
-							output_format = 'mp4'),
-						file_options = dict(
-							output_filename="{FILENAME}_{DATETIMESTAMP}.{OUTPUTFILEEXTENSION}",
-							sync_with_timelapse = True,
-							cleanup_before_print=True,
-							cleanup_after_print=False,
-							cleanup_after_cancel=True,
-							cleanup_before_close=True,
-							cleanup_after_render=False,
-							),
-
-						camera = dict(
-							brightness=128,
-							contrast=128,
-							saturation=128,
-							white_balance_auto= True,
-							gain= 0,
-							powerline_frequency=60,
-							white_balance_temperature= 4000,
-							sharpness= 128,
-							backlight_compensation_enabled= False,
-							exposure_type= True,
-							exposure= 250,
-							exposure_auto_priority_enabled = true,
-							pan=0,
-							tilt=0,
-							autofocus_enabled= True,
-							focus=35,
-							zoom=100,
-							led1_mode= 'auto',
-							led1_frequency= 0,
-							jpeg_quality= 80))
-						});
+		return dict(
+			enabled = True,
+			selected_profile_index = 1,
+			profiles=
+			[
+				dict(
+					name= "default",
+					description= "Fixed XY at back left - relative stabilization (0,100)",
+					printer = dict(
+						bed_x_max= -1, 
+						bed_y_max= -1, 
+						bed_z_max= -1
+					),
+					stabilization = dict(
+						x_movement_speed= 0,
+						x_type = 'fixed',
+						x_fixed_coordinate = 0,
+						x_fixed_path= [],
+						x_fixed_path_loop= True,
+						x_relative= 100,
+						x_relative_print= 100,
+						x_relative_path= [],
+						x_relative_path_loop= True,
+						y_movement_speed_mms= 0, 
+						y_type = 'fixed',
+						y_fixed_coordinate = 0,
+						y_fixed_path= [],
+						y_fixed_path_loop= True,
+						y_relative= 100,
+						y_relative_print= 100,
+						y_relative_path= [],
+						y_relative_path_loop= True,
+						z_movement_speed_mms= 0
+					),
+					snapshot =dict(
+						trigger_type = 'layer_change',
+						length = 0.2,
+						seconds = 30,
+						archive = True,
+						delay = 1000,
+						retract_before_move= False
+					),
+					rendering = dict(
+						enabled = True,
+						fps_calculation_type = 'duration',
+						run_length_seconds = 10,
+						fps = 30,
+						max_fps = 120.0,
+						min_fps = 1.0,
+						output_format = 'mp4'
+					),
+					file_options = dict(
+						output_filename="{FILENAME}_{DATETIMESTAMP}.{OUTPUTFILEEXTENSION}",
+						sync_with_timelapse = True,
+						cleanup_before_print=True,
+						cleanup_after_print=False,
+						cleanup_after_cancel=True,
+						cleanup_before_close=True,
+						cleanup_after_render=False
+					),
+					camera = dict(
+						brightness=128,
+						contrast=128,
+						saturation=128,
+						white_balance_auto= True,
+						gain= 0,
+						powerline_frequency=60,
+						white_balance_temperature= 4000,
+						sharpness= 128,
+						backlight_compensation_enabled= False,
+						exposure_type= True,
+						exposure= 250,
+						exposure_auto_priority_enabled = True,
+						pan=0,
+						tilt=0,
+						autofocus_enabled= True,
+						focus=35,
+						zoom=100,
+						led1_mode= 'auto',
+						led1_frequency= 0,
+						jpeg_quality= 80
+					)
+				)
+			]
+		);
 				
 					
 
@@ -100,14 +112,14 @@ class OctolapsePlugin(octoprint.plugin.SettingsPlugin,
 		# core UI here.
 		return dict(js = ["js/octolapse.js"],
 			css = ["css/octolapse.css"],
-			less = ["less/octolapse.less"]
-		)
+			less = ["less/octolapse.less"])
 
 	##~~ Softwareupdate hook
 
 	def get_update_information(self):
 		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# Plugin here.  See
+		# https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
 		# for details.
 		return dict(octolapse = dict(displayName="Octolapse Plugin",
 				displayVersion=self._plugin_version,
@@ -119,13 +131,15 @@ class OctolapsePlugin(octoprint.plugin.SettingsPlugin,
 				current=self._plugin_version,
 
 				# update method: pip
-				pip="https://github.com/FormerLurker/Octolapse/archive/{target_version}.zip")
-		)
+				pip="https://github.com/FormerLurker/Octolapse/archive/{target_version}.zip"))
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+# If you want your plugin to be registered within OctoPrint under a different
+# name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here.  Same goes for the
+# other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties.  See the
+# documentation for that.
 __plugin_name__ = "Octolapse Plugin"
 
 def __plugin_load__():
