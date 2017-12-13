91933c35eb15e3831946e5e71a3c43938bc56b40
Brad Hochgesang <hochgebe@gmail.com>
2017-12-13 09:22:38 -0600

    Added Invert Loop to stabilization control template and viewmodel.  Fixed problem retrieving x_relative_path_loop from settings

--- octoprint_octolapse/static/js/octolapse.profiles.stabilization.js
+++ octoprint_octolapse/static/js/octolapse.profiles.stabilization.js
@@ -1,6 +1,6 @@
 /// Create our stabilizations view model
 $(function() {
-    Octolapse.StabilizationProfileViewModel = function(values) {
+    Octolapse.StabilizationProfileViewModel = function (values) {
         self = this
         self.name = ko.observable(values.name);
         self.guid = ko.observable(values.guid);
@@ -9,21 +9,26 @@ $(function() {
         self.x_fixed_coordinate = ko.observable(values.x_fixed_coordinate);
         self.x_fixed_path = ko.observable(values.x_fixed_path);
         self.x_fixed_path_loop = ko.observable(values.x_fixed_path_loop);
+        self.x_fixed_path_invert_loop = ko.observable(values.x_fixed_path_invert_loop);
         self.x_relative = ko.observable(values.x_relative);
         self.x_relative_print = ko.observable(values.x_relative_print);
         self.x_relative_path = ko.observable(values.x_relative_path);
         self.x_relative_path_loop = ko.observable(values.x_relative_path_loop);
+        self.x_relative_path_invert_loop = ko.observable(values.x_relative_path_invert_loop);
         self.y_movement_speed_mms = ko.observable(values.y_movement_speed_mms);
         self.y_type = ko.observable(values.y_type);
         self.y_fixed_coordinate = ko.observable(values.y_fixed_coordinate);
         self.y_fixed_path = ko.observable(values.y_fixed_path);
         self.y_fixed_path_loop = ko.observable(values.y_fixed_path_loop);
+        self.y_fixed_path_invert_loop = ko.observable(values.y_fixed_path_invert_loop);
         self.y_relative = ko.observable(values.y_relative);
         self.y_relative_print = ko.observable(values.y_relative_print);
         self.y_relative_path = ko.observable(values.y_relative_path);
         self.y_relative_path_loop = ko.observable(values.y_relative_path_loop);
+        self.y_relative_path_invert_loop = ko.observable(values.y_relative_path_invert_loop);
         self.z_movement_speed_mms = ko.observable(values.z_movement_speed_mms);
-    }
+    };
+
     Octolapse.StabilizationProfileValidationRules = {
         rules: {
             name: "required"
