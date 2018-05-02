894b0a5c2c5ab92faa1840350d0919a4da61ab11
Wesley Heung <wesley@heungs.com>
2018-05-01 23:52:08 -0400

    Added a test to test rendering with watermark.

--- octoprint_octolapse/test/test_render.py
+++ octoprint_octolapse/test/test_render.py
@@ -23,13 +23,14 @@
 import os
 import os.path
 import unittest
+import uuid
 from Queue import Queue
 from shutil import rmtree
 from tempfile import mkdtemp, NamedTemporaryFile
 
 from PIL import Image
 
-from octoprint_octolapse.render import TimelapseRenderJob, Render
+from octoprint_octolapse.render import TimelapseRenderJob, Render, Rendering
 from octoprint_octolapse.settings import OctolapseSettings
 from octoprint_octolapse.utility import get_snapshot_filename, SnapshotNumberFormat
 
@@ -50,43 +51,55 @@ class TestRender(unittest.TestCase):
             image.save(image_path, 'JPEG')
         return dir
 
-    def setUp(self):
-        job_id = "job_id"
-        settings = OctolapseSettings(NamedTemporaryFile().name)
+    @staticmethod
+    def createWatermark():
+        """Create a watermark image in a temp file. The file will be deleted when the handle is garbage collected.
+        Returns the (temporary) file handle."""
+        # Make the temp file.
+        watermark_file = NamedTemporaryFile()
+        # Make an images and save it to the temp file.
+        image = Image.new('RGB', size=(50, 50), color=(155, 0, 0))
+        image.save(watermark_file, 'JPEG')
+        return watermark_file
 
-        print_name = "print_name"
-        print_start_time = 0
-        print_end_time = 100
-
-        # Create fake snapshots.
-        capture_template = get_snapshot_filename(print_name, print_start_time, SnapshotNumberFormat)
-        self.snapshot_dir_path = TestRender.createSnapshotDir(10, capture_template)
-        self.data_directory = mkdtemp()
-        self.octoprint_timelapse_folder = mkdtemp()
-
-        ffmpeg_path = "ffmpeg"
-        render_task_queue = Queue(maxsize=1)
-        render_task_queue.put(job_id)
-
-        self.job = TimelapseRenderJob(job_id=job_id,
-                                      rendering=None,
-                                      debug=settings.current_debug_profile(),
-                                      print_filename=print_name,
+    def createRenderingJob(self, rendering):
+        self.job = TimelapseRenderJob(job_id=self.rendering_job_id,
+                                      rendering=rendering,
+                                      debug=self.octolapse_settings.current_debug_profile(),
+                                      print_filename=self.print_name,
                                       capture_dir=self.snapshot_dir_path,
-                                      capture_template=capture_template,
+                                      capture_template=self.capture_template,
                                       output_tokens=Render._get_output_tokens(self.data_directory, "COMPLETED",
-                                                                              print_name,
-                                                                              print_start_time,
-                                                                              print_end_time),
+                                                                              self.print_name,
+                                                                              self.print_start_time,
+                                                                              self.print_end_time),
                                       octoprint_timelapse_folder=self.octoprint_timelapse_folder,
-                                      ffmpeg_path=ffmpeg_path,
+                                      ffmpeg_path=self.ffmpeg_path,
                                       threads=1,
-                                      rendering_task_queue=render_task_queue,
+                                      rendering_task_queue=self.render_task_queue,
                                       time_added=0,
                                       on_render_start=lambda job_id, payload: None,
                                       on_complete=lambda job_id, payload: None,
                                       clean_after_success=True,
                                       clean_after_fail=True)
+
+    def setUp(self):
+        self.rendering_job_id = "job_id"
+        self.octolapse_settings = OctolapseSettings(NamedTemporaryFile().name)
+
+        self.print_name = "print_name"
+        self.print_start_time = 0
+        self.print_end_time = 100
+
+        # Create fake snapshots.
+        self.capture_template = get_snapshot_filename(self.print_name, self.print_start_time, SnapshotNumberFormat)
+        self.snapshot_dir_path = TestRender.createSnapshotDir(10, self.capture_template)
+        self.data_directory = mkdtemp()
+        self.octoprint_timelapse_folder = mkdtemp()
+
+        self.ffmpeg_path = "ffmpeg"
+        self.render_task_queue = Queue(maxsize=1)
+        self.render_task_queue.put(self.rendering_job_id)
 
     def tearDown(self):
         rmtree(self.snapshot_dir_path)
@@ -94,6 +107,21 @@ class TestRender(unittest.TestCase):
         rmtree(self.octoprint_timelapse_folder)
 
     def test_basicRender(self):
+        # Create the job.
+        self.createRenderingJob(rendering=None)
+
+        # Start the job.
+        self.job.process()
+
+        # Wait for the job to finish.
+        self.job._thread.join()
+
+    def test_watermark(self):
+        # Create the job.
+        r = Rendering(guid=uuid.uuid4(), name="Render with Watermark")
+        r.update({"watermark": True})
+        self.createRenderingJob(rendering=r)
+
         # Start the job.
         self.job.process()
 
