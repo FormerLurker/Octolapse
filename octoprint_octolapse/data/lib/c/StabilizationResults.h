d2fbcff2642fb70ac167a4b4fd48f31d409e1179
Brad Hochgesang <hochgebe@gmail.com>
2019-03-03 07:52:05 -0600

    Added two missing c++ files to the GcodePositionProcessor.

---
+++ octoprint_octolapse/data/lib/c/StabilizationResults.h
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Octolapse - A plugin for OctoPrint used for making stabilized timelapse videos.
+// Copyright(C) 2019  Brad Hochgesang
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This program is free software : you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.If not, see the following :
+// https ://github.com/FormerLurker/Octolapse/blob/master/LICENSE
+//
+// You can contact the author either through the git - hub repository, or at the
+// following email address : FormerLurker@pm.me
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef STABILIZATION_RESULTS_H
+#define STABILIZATION_RESULTS_H
+#include <string>
+#include <vector>
+#include "SnapshotPlan.h"
+class stabilization_results
+{
+public:
+	stabilization_results();
+	~stabilization_results();
+
+	bool success;
+	std::string errors;
+	std::vector<snapshot_plan*>* p_snapshot_plans;
+	double seconds_elapsed;
+	long gcodes_processed;
+	long lines_processed;
+};
+
+#endif
