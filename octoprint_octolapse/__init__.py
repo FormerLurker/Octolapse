f9645c2047a9702d8b8db76185d1815d01c3796a
Formerlurker <formerlurker@pm.me>
2020-03-17 13:34:48 -0500

    Attempt to fix git version info when installing from github generated zip.

--- octoprint_octolapse/__init__.py
+++ octoprint_octolapse/__init__.py
@@ -3709,6 +3709,10 @@ class OctolapsePlugin(
 
 from ._version import get_versions
 __version__ = get_versions()['version']
+__git_version__ = get_versions()['full-revisionid']
+# work around if zip is downloaded from github and current version does not have a tag.
+if __version__ == '0+unknown':
+    __version__ = __git_version__
 del get_versions
 
 __plugin_name__ = "Octolapse"
