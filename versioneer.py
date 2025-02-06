c5b119a712da5e2f8e7940b33499d9c5d1595901
SvenTh99 <sthiede@gmx.de>
2025-02-06 23:31:31 +0100

    Update versioneer.py

--- versioneer.py
+++ versioneer.py
@@ -341,7 +341,7 @@ def get_config_from_root(root):
     setup_cfg = os.path.join(root, "setup.cfg")
     parser = configparser.ConfigParser()
     with open(setup_cfg, "r") as f:
-        parser.readfp(f)
+            parser.read_file(f)
     VCS = parser.get("versioneer", "VCS")  # mandatory
 
     def get(parser, name):
